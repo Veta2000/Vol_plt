0408aa265b8ae8743b8146ed8dcb03abe6b112c8
Veta2000 <elizter121@gmail.com>
2024-11-07 13:35:04 +0200

    404

---
+++ backend/404.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'includes/navbar.php';
+?>
+
+<div class="container mt-5">
+    <h1>Ошибка 404</h1>
+    <p>Страница не найдена. Пожалуйста, проверьте URL или вернитесь на <a href="/index.php">главную страницу</a>.</p>
+</div>
+
+<?php include_once 'includes/footer.php'; ?>
